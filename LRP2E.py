243550660d75296d674132e8fc4b9aa80f0ea735
guoph <guopenghui668@gmail.com>
2017-12-15 21:37:15 +0800

    When a pop is created, the pop is sorted. Modify the single_objective_evolution function.

--- LRP2E.py
+++ LRP2E.py
@@ -21,17 +21,18 @@ SATELLITE = {i:((random.uniform(0, 10), random.uniform(0, 10)), 1000)
              for i in range(max(DEPOT) + 1, max(DEPOT) + 1 + SATELLITE_NUM)}
 CUSTOMER  = {i:((random.uniform(0, 10), random.uniform(0, 10)), [20] * DEPOT_NUM)
              for i in range(max(SATELLITE) + 1, max(SATELLITE) + 1 + CUSTOMER_NUM)}
-
-
 INSTANCE = {'depot': DEPOT, 'satellite': SATELLITE, 'customer': CUSTOMER,
             'vehicle1_num': VEHICLE1_NUM, 'vehicle2_num': VEHICLE2_NUM,
             'vehicle1_cap': VEHICLE1_CAP, 'vehicle2_cap': VEHICLE2_CAP,
             'satellite_cap': SATELLITE_CAP}
-PARAMETERS = {'pop_size': 500, 'offspring_size': 30, 'archive_size': 300,
+# parameters
+PARAMETERS = {'pop_size': 500, 'offspring_size': 300,
+              'archive_size': 300, 'k': 100,
               'obj_num': 3, 'f': 0.05,
               'mutt_prob': 0.05, 'cross_prob': 0.5,
               'violation_weigh': 0.5,
-              'not_feasible_weigh': {'depot':0.2, 'satellite':0.2, 'customer':0.2, 'vehicle':0.4}}
+              'not_feasible_weigh': {'depot':0.2, 'satellite':0.2, 'customer':0.2, 'vehicle':0.4},
+              'iter_times': 10}
 
 class VRP2E:
     def __init__(self, instance, parameters):
@@ -46,11 +47,12 @@ class VRP2E:
                 self.loc[i] = data[i][0]
 
         # evolutionary algorithm parameters
+        self.iter_times = parameters['iter_times']
         self.pop_size = parameters['pop_size']
         self.offspring_size = parameters['offspring_size']
         self.archive_size = parameters['archive_size']
         self.obj_num = parameters['obj_num']
-        self.k = 100 # self.pop_size
+        self.k = parameters['k'] # self.pop_size
         self.f = parameters['f']
         self.mutt_prob = parameters['mutt_prob']
         self.cross_prob = parameters['cross_prob']
@@ -130,52 +132,57 @@ class VRP2E:
 
         for key in assignment:
             for i in range(len(self.depot)):
-                assignment[key].append(random.randrange(0,20))
+                assignment[key].append(random.randrange(0,20))  # 17.12.15
 
         d_s = self.depot_satellite_route(assignment)
         s_c = self.satellite_customer_route(assignment)
         individual = [assignment, d_s, s_c]
-        individual += [[self.obj_time(individual),
-                       self.obj_satisfaction_equity(individual)[0],
-                       self.obj_satisfaction_equity(individual)[1]]]
+        individual += [self.obj_value(individual)]
         individual += [self.not_feasible(individual)]
         return(individual)
 
-    def rand_pop(self):
-        return([self.rand_ind() for _ in range(self.pop_size)])
+    def rand_pop(self, obj_index):
+        pop = [self.rand_ind() for _ in range(self.pop_size)]
+        sorted_pop = sorted(pop, key=lambda ind: ind[3][obj_index])
+        return(sorted_pop)
 
-    def obj_time(self, ind):
-        obj_value = 0
-        d_s, s_c = ind[1], ind[2]
-        assert(set(d_s) & set(s_c) == set())
-        temp_dic = copy.deepcopy(d_s)
-        temp_dic.update(s_c)
+    def obj_value(self, ind):
+        def obj_time(self, ind):
+            obj_value = 0
+            d_s, s_c = ind[1], ind[2]
+            assert(set(d_s) & set(s_c) == set())
+            temp_dic = copy.deepcopy(d_s)
+            temp_dic.update(s_c)
 
-        for key in temp_dic:
-            for li in temp_dic[key]:
-                temp_li = [key] + li + [key]
-                for i in range(len(temp_li) - 1):
-                    obj_value += math.sqrt(
-                        (self.loc[temp_li[i]][0] - self.loc[temp_li[i + 1]][0]) ** 2
-                      + (self.loc[temp_li[i]][1] - self.loc[temp_li[i + 1]][1]) ** 2)
-        return(obj_value)
+            for key in temp_dic:
+                for li in temp_dic[key]:
+                    temp_li = [key] + li + [key]
+                    for i in range(len(temp_li) - 1):
+                        obj_value += math.sqrt(
+                            (self.loc[temp_li[i]][0] - self.loc[temp_li[i + 1]][0]) ** 2
+                          + (self.loc[temp_li[i]][1] - self.loc[temp_li[i + 1]][1]) ** 2)
+            return(obj_value)
 
-    def customer_satisfaction(self, ind):
-        assign = ind[0]
-        customer_satisfaction_dic = {i: [0] * len(self.depot) for i in self.customer}
+        def customer_satisfaction(self, ind):
+            assign = ind[0]
+            customer_satisfaction_dic = {i: [0] * len(self.depot) for i in self.customer}
 
-        for key in assign:
-            for j in range(len(self.depot)):
-                customer_satisfaction_dic[key][j] = assign[key][j+1] / self.customer[key][1][j]
+            for key in assign:
+                for j in range(len(self.depot)):
+                    customer_satisfaction_dic[key][j] = assign[key][j+1] / self.customer[key][1][j]
 
-        customer_satisfaction_dic = {i:[customer_satisfaction_dic[i], sum(customer_satisfaction_dic[i])]
-                                     for i in customer_satisfaction_dic}  # different production share same weight
-        return(customer_satisfaction_dic)
+            customer_satisfaction_dic = {i:[customer_satisfaction_dic[i], sum(customer_satisfaction_dic[i])]
+                                         for i in customer_satisfaction_dic}  # different production share same weight
+            return(customer_satisfaction_dic)
 
-    def obj_satisfaction_equity(self, ind):
-        customer_satisfaction_dic = self.customer_satisfaction(ind)
-        temp_li = [customer_satisfaction_dic[i][1] for i in customer_satisfaction_dic]
-        return(-np.sum(temp_li), np.var(temp_li))
+        def obj_satisfaction_equity(self, ind):
+            customer_satisfaction_dic = customer_satisfaction(self, ind)
+            temp_li = [customer_satisfaction_dic[i][1] for i in customer_satisfaction_dic]
+            return(-np.sum(temp_li), np.var(temp_li))
+
+        obj_t = obj_time(self, ind)
+        obj_s_e = obj_satisfaction_equity(self, ind)
+        return([obj_t, obj_s_e[0], obj_s_e[1]])
 
     def crossover(self, ind0, ind1):
         assignment0, assignment1 = copy.deepcopy(ind0[0]), copy.deepcopy(ind1[0])
@@ -192,19 +199,22 @@ class VRP2E:
         satellite_customer_route_1 = self.satellite_customer_route(assignment1)
         ind0_son = [assignment0, depot_satellite_route_0, satellite_customer_route_0]
         ind1_son = [assignment1, depot_satellite_route_1, satellite_customer_route_1]
-        ind0_son += [[self.obj_time(ind0_son), self.obj_satisfaction_equity(ind0_son)[0],
-                     self.obj_satisfaction_equity(ind0_son)[1]]]
-        ind1_son += [[self.obj_time(ind1_son), self.obj_satisfaction_equity(ind1_son)[0],
-                     self.obj_satisfaction_equity(ind1_son)[1]]]
+        ind0_son += [self.obj_value(ind0_son)]
+        ind1_son += [self.obj_value(ind1_son)]
         ind0_son += [self.not_feasible(ind0_son)]
         ind1_son += [self.not_feasible(ind1_son)]
         return(ind0_son, ind1_son)
 
-    def mutation(self, ind, pop, pop_best=[], archive_best=[], f=0.05):
-        pop_best, archive_best = pop[0], random.choice(pop)  # just for test
+    def mutation(self, ind, pop, archive, f=0.05):
+        pop_best = random.choice(pop)
+        for i in pop:
+            if not i[4]:
+                pop_best = i[:]
+                break
+        archive_best = random.choice(archive)  # just for test
 
-        ind1_assignment, ind2_assignment = random.choice(pop)[0], random.choice(pop)[0]
         pop_best_assignment, archive_best_assignment = pop_best[0], archive_best[0]
+        ind1_assignment, ind2_assignment = random.choice(pop)[0], random.choice(pop)[0]
         ind_assignment = ind[0]
         # new_assignment = OrderedDict({key: ind_assignment[key][:] for key in ind_assignment})
         new_assignment = copy.deepcopy(ind_assignment)
@@ -227,8 +237,7 @@ class VRP2E:
         depot_satellite_route = self.depot_satellite_route(new_assignment)
         satellite_customer_route = self.satellite_customer_route(new_assignment)
         new_ind = [new_assignment, depot_satellite_route, satellite_customer_route]
-        new_ind += [[self.obj_time(new_ind), self.obj_satisfaction_equity(new_ind)[0],
-                    self.obj_satisfaction_equity(new_ind)[1]]]
+        new_ind += [self.obj_value(new_ind)]
         new_ind += [self.not_feasible(new_ind)]
         return(new_ind)
 
@@ -293,9 +302,10 @@ class VRP2E:
                 chosen_one = ind1[:]
         return(chosen_one)
 
-    def single_objective_selection(self, obj_index, ind1, ind2, pop):
+    def single_objective_selection(self, obj_index, ind1, ind2, pop, archive):
+        # input 2 ind, output 2 ind
         if random.random() < self.mutt_prob:
-            temp_ind1, temp_ind2 = self.mutation(ind1, pop), self.mutation(ind2, pop)
+            temp_ind1, temp_ind2 = self.mutation(ind1, pop, archive), self.mutation(ind2, pop, archive)
         else:
             temp_ind1, temp_ind2 = ind1[:], ind2[:]
         temp_pair = self.crossover(temp_ind1, temp_ind2)
@@ -304,23 +314,46 @@ class VRP2E:
         offspring.append(self.constraint_choose(obj_index, temp_pair[1], ind2))
         return(offspring)
 
-    def single_objective_evolution(self, obj_index, pop):
-        # mu + lambda evolution strategy
+    def single_objective_evolution(self, obj_index, pop, archive):
+        # mu + lambda evolution strategy, the best one is preserved.
+        # mu ~ pop_size, lambda ~ offspring_size
         # input: population & evaluate function
         # output: the best feasible individual & offspring population
+        temp_pop = pop[:]
         offspring_population = []
-        pairs = [random.sample(pop, 2) for _ in range(int(self.offspring_size / 2))]
-        for pair in pairs[0:int(self.offspring_size/2)]:
-            for a in self.single_objective_selection(obj_index, pair[0], pair[1], pop):
+        chose_ind = []
+        for _ in range(int(self.offspring_size / 2)):
+            ind = random.choice(temp_pop)
+            chose_ind.append(ind[:])
+            temp_pop.remove(ind)
+        pairs = []
+        while chose_ind != []:
+            ind0 = random.choice(chose_ind)[:]
+            chose_ind.remove(ind0)
+            ind1 = random.choice(chose_ind)[:]
+            chose_ind.remove(ind1)
+            pairs.append((ind0, ind1))
+        for pair in pairs:
+            for a in self.single_objective_selection(obj_index, pair[0], pair[1], pop, archive):
                 offspring_population.append(a)
-        temp_ind_li = pop + offspring_population
-        temp_ind_li.sort(key=lambda ind: ind[3][obj_index])
-        # TODO return the best k 'feasible!' individual
-        res = []
-        for ind in temp_ind_li:
-            if not ind[4]: res.append(ind)
-            if len(res) >= self.k: break
-        return(res)
+        new_pop = temp_pop + offspring_population
+
+        sorted_new_pop = sorted(new_pop, key=lambda ind: ind[3][obj_index])
+        # preserve the best feasible ind
+        sorted_new_pop.remove(sorted_new_pop[-1])
+        for ind in sorted_new_pop:
+            if not ind[4]:
+                sorted_new_pop.append(ind)
+                break
+        sorted_new_pop = sorted(sorted_new_pop, key=lambda ind: ind[3][obj_index])
+        # archive the k best feasible ind
+        k_best = []
+        for ind in sorted_new_pop:
+            if not ind[4]:
+                k_best.append(ind)
+            if len(k_best) >= self.k:
+                break
+        return(k_best, sorted_new_pop)
 
     def a_dominate_b(self, ind_a, ind_b):
         for i in range(len(ind_a[3])):
@@ -351,18 +384,17 @@ class VRP2E:
         educated_ind = []
         return(educated_ind)
 
-    def multi_objective_evolution(self, best_k_s):
+    def multi_objective_evolution(self, archive, best_k_s):
         # chose the best k ind in every species, and put them into the archive set.
         # The 'education method' is applied to the individuals in archive set.
         # The archive set is separated into subsets of dominated one and non-dominated one.
-        temp_archive = []
-        for li in best_k_s:
-            temp_archive += li
+        temp_archive = archive + best_k_s
         res = self.non_dominated_set(temp_archive)
         non_dominated_ind, dominated_ind = res[0], res[1]
         if len(non_dominated_ind) <= self.archive_size:
             return(non_dominated_ind)
         else:
+            # TODO select according to 'crowd distance' proposed by Deb
             return(non_dominated_ind[:self.archive_size])
 
 
@@ -377,10 +409,22 @@ def timer(func):
 @timer
 def main():
     v = VRP2E(INSTANCE, PARAMETERS)
-    ini_pop = v.rand_pop()
-    n = v.single_objective_evolution(0, ini_pop)
-    print(len(n))
+    non_dominated_archive = []
+    best_k_s = []
+    for i in range(3):
+        ini_pop = v.rand_pop(i)
+        obj_i_best_k, single_objective_offspring = v.single_objective_evolution(i, ini_pop, ini_pop)
+        best_k_s += obj_i_best_k
+    non_dominated_archive = v.multi_objective_evolution(non_dominated_archive, best_k_s)
 
+    for _ in range(v.iter_times):
+        best_k_s = []
+        for i in range(3):
+            obj_i_best_k, single_objective_offspring = v.single_objective_evolution(i, single_objective_offspring, non_dominated_archive)
+            best_k_s += obj_i_best_k
+        temp_li = non_dominated_archive[:]
+        non_dominated_archive = v.multi_objective_evolution(non_dominated_archive, best_k_s)
+        print(len([a for a in non_dominated_archive if a not in temp_li]), len(non_dominated_archive))
 
 
 if __name__ == '__main__':
